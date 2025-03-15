689b3ec2e23df301d197a09d31d0b703d4a66bca
Knot <knotleo1234@gmail.com>
2025-03-16 02:10:01 +0700

    Change class name from PlayerInputController to InputController

--- Assets/Module-Player/InputSystem.cs
+++ Assets/Module-Player/InputSystem.cs
@@ -3,7 +3,7 @@ using UnityEngine.InputSystem;
 
 namespace FpsGame.Player
 {
-    public class PlayerInputSystem : MonoBehaviour
+    public class InputSystem : MonoBehaviour
     {
         private PlayerInputAction inputAction;
         [SerializeField] Vector2 lookInput;
@@ -11,7 +11,6 @@ namespace FpsGame.Player
         public Vector2 LookInput { get => lookInput; private set => lookInput = value; }
         public Vector2 MoveInput { get => moveInput; private set => moveInput = value; }
         public PlayerInputAction InputAction { get => inputAction; private set => inputAction = value; }
-
         private void Awake()
         {
             InputAction = new PlayerInputAction();
@@ -32,12 +31,10 @@ namespace FpsGame.Player
             InputAction.Player.Move.canceled += OnMove;
             InputAction.Disable();
         }
-
         private void OnLook(InputAction.CallbackContext context)
         {
             lookInput = context.ReadValue<Vector2>();
         }
-
         private void OnMove(InputAction.CallbackContext context)
         {
             moveInput = context.ReadValue<Vector2>();
