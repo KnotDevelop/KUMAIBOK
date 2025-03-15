ed86b2008996493c41edc284b0945a17fe73641c
Knot <knotleo1234@gmail.com>
2025-03-15 22:45:05 +0700

    Combine MovementCotroller with CamaraController

--- Assets/Module-Player/MovementController.cs
+++ Assets/Module-Player/MovementController.cs
@@ -5,17 +5,39 @@ namespace FpsGame.Player.Camera
 {
     public class MovementController : MonoBehaviour
     {
-        [SerializeField] Transform m_Camera;
-        PlayerInputSystem m_Input;
-        Rigidbody m_Rigidbody;
-        [SerializeField] float speed = 5f;
+        [SerializeField]
+        private Transform m_Camera;
+        private PlayerInputSystem m_Input;
+        private Rigidbody m_Rigidbody;
+        [SerializeField]
+        private float speed = 5f;
+        [SerializeField]
+        private float m_SmoothTime = 40f;
 
         private void Start()
         {
             m_Input = GetComponent<PlayerInputSystem>();
             m_Rigidbody = GetComponent<Rigidbody>();
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        private void LateUpdate()
+        {
+            HandleMovement();
         }
         private void FixedUpdate()
+        {
+            HandleRotation();
+        }
+
+        private void HandleMovement()
+        {
+            Vector3 forward = m_Camera.forward;
+            forward.y = 0f;
+            transform.forward = Vector3.Slerp(transform.forward, forward, Time.deltaTime * m_SmoothTime);
+        }
+
+        private void HandleRotation()
         {
             Vector3 move = m_Camera.forward * m_Input.MoveInput.y + m_Camera.right * m_Input.MoveInput.x;
             move.y = 0;
