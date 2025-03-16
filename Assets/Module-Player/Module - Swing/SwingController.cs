dc5f87895031f76756f5831c2dd32a2ffc8cdbb0
Eakrawi <eakrawichaisuriya@gmail.com>
2025-03-16 16:40:31 +0700

    Swing controller v1

--- Assets/Module-Player/Module - Swing/SwingController.cs
+++ Assets/Module-Player/Module - Swing/SwingController.cs
@@ -6,74 +6,126 @@ using UnityEngine.InputSystem;
 
 public class SwingController : MonoBehaviour
 {
+    /// <summary>
+    /// The amount of dot in line renderer (positionCount)
+    /// </summary>
     const int lineResolution = 10;
 
+    /// <summary>
+    /// Start position of the line
+    /// </summary>
     [SerializeField]
     Transform startPoint;
 
+    /// <summary>
+    /// Amount of jittering at the beginning of Swing
+    /// </summary>
     [SerializeField]
     AnimationCurve flickerCurve;
 
+    /// <summary>
+    /// Line renderer component in Unity
+    /// </summary>
     [SerializeField]
     LineRenderer line;
 
+    /// <summary>
+    /// Input for Shooting swing line
+    /// </summary>
     [SerializeField]
     InputAction shoot;
+    private Coroutine instanceCoroutine;
+    private List<Coroutine> dotInstancesCoroutine = new List<Coroutine>();
 
-    [SerializeField]
-    InputAction jumpboost;
-    Coroutine instanceCoroutine;
-    List<Coroutine> dotInstancesCoroutine = new List<Coroutine>();
-
+    /// <summary>
+    /// Speed of shooting of swing line.
+    /// </summary>
     [SerializeField]
     [Range(0, 3)]
     float speed = 2;
+
+    /// <summary>
+    /// the current swinging point (target position)
+    /// </summary>
     Vector3 swingPoint;
 
+    /// <summary>
+    /// force added of the starting of swing. Jump Boost
+    /// </summary>
     [SerializeField]
-    float upForce = 5;
+    float upBoostForce = 5;
 
+    /// <summary>
+    /// boosting force applied continously during swing.
+    /// </summary>
     [SerializeField]
     float boostForce = 30;
 
+    /// <summary>
+    /// rigidbody unity Component
+    /// </summary>
     [SerializeField]
-    Rigidbody rb;
+    Rigidbody rigidBody;
 
     void Start()
     {
         shoot.Enable();
-        jumpboost.Enable();
     }
 
     void Update()
     {
+        InputHandler();
+        UpdateCurrentPosition();
+    }
+    /// <summary>
+    /// Apply event on each input.
+    /// </summary>
+    private void InputHandler()
+    {
         if (shoot.WasPressedThisFrame())
         {
-            if (instanceCoroutine != null)
-            {
-                StopCoroutine(instanceCoroutine);
-            }
-            foreach (var d in dotInstancesCoroutine)
-            {
-                StopCoroutine(d);
-            }
-            dotInstancesCoroutine.Clear();
-            line.positionCount = 1;
+            ResetSwing();
             instanceCoroutine = StartCoroutine(FlickerLine());
             JumpToSwingPoint();
         }
-
         if (shoot.IsPressed())
         {
             BoostToSwingPoint();
         }
         if (shoot.WasReleasedThisFrame())
         {
-            line.positionCount = 1;
+            ResetSwing();
         }
+    }
+    /// <summary>
+    /// Update the first dot on line renderer to current position
+    /// </summary>
+    void UpdateCurrentPosition()
+    {
         line.SetPosition(0, startPoint.position);
     }
 
+    /// <summary>
+    /// Reset the swing coroutine.
+    /// </summary>
+    private void ResetSwing()
+    {
+        if (instanceCoroutine != null)
+        {
+            StopCoroutine(instanceCoroutine);
+        }
+        foreach (var d in dotInstancesCoroutine)
+        {
+            StopCoroutine(d);
+        }
+        dotInstancesCoroutine.Clear();
+        line.positionCount = 1;
+    }
+
+    /// <summary>
+    /// Flicker all dots in line at the start of swing for aesthetic purpose.
+    /// </summary>
+    /// <returns></returns>
     IEnumerator FlickerLine()
     {
         line.positionCount = lineResolution;
@@ -98,6 +150,12 @@ public class SwingController : MonoBehaviour
         instanceCoroutine = null;
     }
 
+    /// <summary>
+    /// Flicker the dot in line to create aesthetic purpose.
+    /// </summary>
+    /// <param name="i"></param>
+    /// <param name="endPoint"></param>
+    /// <returns></returns>
     IEnumerator FlickerSingleDot(int i, Vector3 endPoint)
     {
         float lerp = 0;
@@ -111,17 +169,23 @@ public class SwingController : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Continous applied force during swing to the target position.
+    /// </summary>
     private void BoostToSwingPoint()
     {
         Vector3 displacement = swingPoint - transform.position;
-        displacement = displacement.normalized * upForce;
-        rb.AddForce(displacement * Time.deltaTime, ForceMode.VelocityChange);
+        displacement = displacement.normalized * upBoostForce;
+        rigidBody.AddForce(displacement * Time.deltaTime, ForceMode.VelocityChange);
     }
 
+    /// <summary>
+    /// single Jump (appllied force) to at the start of swing for boosting purpose.
+    /// </summary>
     private void JumpToSwingPoint()
     {
         Vector3 displacement = swingPoint - transform.position;
         displacement = displacement.normalized;
-        rb.AddForce(displacement * boostForce, ForceMode.Impulse);
+        rigidBody.AddForce(displacement * boostForce, ForceMode.Impulse);
     }
 }
