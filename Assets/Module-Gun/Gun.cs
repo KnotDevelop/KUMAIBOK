ad05e57323411e7a01cfef34365b2366ff50f62f
Knot <knotleo1234@gmail.com>
2025-03-17 00:43:59 +0700

    add Reload feature

--- Assets/Module-Gun/Gun.cs
+++ Assets/Module-Gun/Gun.cs
@@ -1,12 +1,14 @@
+using System.Collections;
 using UnityEngine;
 namespace FpsGame.Gun
 {
     public enum GunFireMode { Single, Full_Auto, Burst_3_Round }
-    public class Gun
+    public class Gun : MonoBehaviour
     {
         public float damage;
         public float accuracy;
         public float rps;
+        public float currentBullet;
         public float magazineSize;
         public float reloadTime;
         public float recoil;
@@ -14,17 +16,58 @@ namespace FpsGame.Gun
         public float effectiveRange;
         public GunFireMode fireMode;
         public float noise;
-        public bool isSingleReload;
+        public bool oneRoundReload;
 
-        public virtual void Shot()
+        Coroutine reloading_routine;
+
+        public void Shot()
         {
 
         }
-        public virtual void Reload()
+        [ContextMenu("Reload")]
+        public void Reload()
         {
+            bool reloading = reloading_routine != null;
+            bool magazineFull = currentBullet >= magazineSize;
 
+            if (reloading || magazineFull)
+            {
+                Debug.Log("Can't reload");
+                return;
+            }
+
+            reloading_routine = StartCoroutine(Reloading());
         }
-        public virtual void Aim()
+        IEnumerator Reloading()
+        {
+            Debug.Log("Reloading");
+            while (true)
+            {
+                yield return new WaitForSeconds(reloadTime);
+                if (oneRoundReload)
+                {
+                    currentBullet += 1;
+                }
+                else
+                {
+                    //Bullet in inventory - (magazineSize - currentBullet)
+                    currentBullet = magazineSize;
+                }
+
+                if (currentBullet >= magazineSize)
+                    break;
+            }
+            reloading_routine = null;
+            Debug.Log("Reload success");
+        }
+        [ContextMenu("StopReload")]
+        public void StopReload()
+        {
+            if (reloading_routine == null) return;
+            StopCoroutine(reloading_routine);
+            reloading_routine = null;
+        }
+        public void Aim()
         {
 
         }
