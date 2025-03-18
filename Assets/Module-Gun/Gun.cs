2f87bc3112d7f161955d39aaa7e54bdfd90024b0
Knot <knotleo1234@gmail.com>
2025-03-18 23:29:29 +0700

    Gun Full_Auto Shooting System

--- Assets/Module-Gun/Gun.cs
+++ Assets/Module-Gun/Gun.cs
@@ -17,12 +17,84 @@ namespace FpsGame.Gun
         public float noise;
         public bool oneRoundReload;
 
+        [SerializeField] Transform muzzle;
+        private float m_RpsCooldown = 0;
+        [SerializeField]
         private float m_CurrentBullet;
         private Coroutine m_ReloadingRoutine;
+        private Coroutine m_ShootRoutine;
+        private float m_Rps => 1f / rps;
+        private bool m_EmptyMagazine => m_CurrentBullet <= 0;
 
-        public void Shot()
+
+        private void Update()
         {
+            if (Input.GetKeyDown(KeyCode.O))
+            {
+                OnTriggerPull();
+            }
+            if (Input.GetKeyUp(KeyCode.O))
+            {
+                OnTriggerRelease();
+            }
+        }
 
+        [ContextMenu("Shot")]
+        IEnumerator Shooting_FullAuto()
+        {
+            while (true)
+            {
+                //Debug.Log($"{Time.time} {m_RpsCooldown}");
+                if (Time.time > m_RpsCooldown)
+                {
+                    Debug.Log("FullAutoShoot");
+                    HitTargetCheck();
+                    m_CurrentBullet -= 1;
+                    m_RpsCooldown = Time.time + m_Rps;
+                    if (m_EmptyMagazine)
+                        OnTriggerRelease();
+                }
+                yield return new WaitForEndOfFrame();
+            }
+        }
+        void StopShooting()
+        {
+            if (m_ShootRoutine == null) return;
+            StopCoroutine(m_ShootRoutine);
+            m_ShootRoutine = null;
+        }
+        void HitTargetCheck()
+        {
+            Vector3 _muzzle = muzzle.transform.position;
+            RaycastHit hit;
+            if (Physics.Raycast(_muzzle, muzzle.transform.forward, out hit, 10f))
+            {
+                Debug.Log($"{gameObject.name} hit {hit.collider.name}");
+            }
+        }
+        public void OnTriggerPull()
+        {
+            if (m_EmptyMagazine)
+            {
+                Reload();
+                return;
+            }
+
+            StopShooting();
+            switch (fireMode)
+            {
+                case GunFireMode.Single:
+                    break;
+                case GunFireMode.Full_Auto:
+                    m_ShootRoutine = StartCoroutine(Shooting_FullAuto());
+                    break;
+                case GunFireMode.Burst_3_Round:
+                    break;
+            }
+        }
+        public void OnTriggerRelease()
+        {
+            StopShooting();
         }
         [ContextMenu("Reload")]
         public void Reload()
